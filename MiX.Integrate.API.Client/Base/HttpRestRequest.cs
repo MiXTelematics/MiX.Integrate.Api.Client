d7ad4bf3245e28797550338681aa4ec366bf218f
Johannes Engelbrecht <johannes.engelbrecht@mixtelematics.com>
2017-08-28 11:09:13 +0200

    MI-576 parking

---
+++ MiX.Integrate.API.Client/Base/HttpRestRequest.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+
+namespace MiX.Integrate.API.Client.Base
+{
+	public class HttpRestRequest : IHttpRestRequest
+	{
+
+		public string Resource { get; set; }
+		public HttpMethod Method { get; set; }
+		public string JsonBody { get; set; }
+		//public Dictionary<string, string> Headers { get; set; }
+		//public Dictionary<string, string>  QueryParameters { get; set; }
+		//public Dictionary<string, string>  UrlSegments { get; set; }
+
+		public IEnumerable<KeyValuePair<string, string>> Headers { get; set; }
+		public IEnumerable<KeyValuePair<string, string>> QueryParameters { get; set; }
+		public IEnumerable<KeyValuePair<string, string>> UrlSegments { get; set; }
+
+
+		public HttpRestRequest(string resource, HttpMethod method)
+		{
+			Resource = resource;
+			Method = method;
+			Headers = new List<KeyValuePair<string, string>>();
+			QueryParameters = new List<KeyValuePair<string, string>>();
+			UrlSegments = new List<KeyValuePair<string, string>>();
+			JsonBody = string.Empty;
+		}
+
+		public void AddJsonBody(object obj)
+		{ }
+
+		public void AddHeader(string name, string value)
+		{ }
+
+		public void AddQueryParameter(string name, string value)
+		{
+
+		}
+
+		public void AddUrlSegment(string name, string value)
+		{ }
+
+
+	}
+}
---
+++ MiX.Integrate.API.Client/Base/IHttpRestRequest.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MiX.Integrate.API.Client.Base
+{
+	public interface IHttpRestRequest
+	{
+	}
+}
