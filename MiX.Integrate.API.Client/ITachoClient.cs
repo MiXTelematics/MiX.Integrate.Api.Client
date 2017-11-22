e6d5225dd272e62d298b9739b845b76053b1b3d8
Johannes Engelbrecht <johannes.engelbrecht@mixtelematics.com>
2017-11-22 08:47:26 +0200

    MI-463 Addedx Tacho entities and client

---
+++ MiX.Integrate.API.Client/ITachoClient.cs
@@ -0,0 +1,15 @@
+﻿using MiX.Integrate.Api.Client.Base;
+using MiX.Integrate.Shared.Entities.Tacho;
+using System;
+using System.Threading.Tasks;
+
+namespace MiX.Integrate.Api.Client
+{
+	public interface ITachoClient : IBaseClient
+	{
+
+		TachoData GetRangeForAsset(long assetId, DateTime from, DateTime to);
+		Task<TachoData> GetRangeForAssetAsync(long assetId, DateTime from, DateTime to);
+
+	}
+}
