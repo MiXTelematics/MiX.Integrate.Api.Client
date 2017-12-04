12cec341502bda26aa01b6eeead52e1178580ca4
Johannes Engelbrecht <johannes.engelbrecht@mixtelematics.com>
2017-12-04 09:58:03 +0200

    MI-598 Connected peripherals

---
+++ MiX.Integrate.API.Client/DeviceConfigurationClient.cs
@@ -0,0 +1,33 @@
+﻿using MiX.Integrate.Api.Client;
+using MiX.Integrate.Api.Client.Base;
+using MiX.Integrate.Shared.Constants;
+using MiX.Integrate.Shared.Entities.DeviceConfiguration;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace MiX.Integrate.API.Client
+{
+	public class DeviceConfigurationClient : BaseClient, IDeviceConfigurationClient
+	{
+		public DeviceConfigurationClient(string url, bool setTestRequestHeader = false) : base(url, setTestRequestHeader) { }
+		public DeviceConfigurationClient(string url, IdServerResourceOwnerClientSettings settings, bool setTestRequestHeader = false) : base(url, settings, setTestRequestHeader) { }
+
+		public async Task<List<ConfigurationGroupDetail>> GetConnectedPeripheralsForAssetsAsync(long groupId, List<long> assetIds)
+		{
+			IHttpRestRequest request = GetRequest(APIControllerRoutes.DEVICECONFIGCONTROLLER.GETCONNECTEDPERIPHERALSFORASSETS, HttpMethod.Post);
+			request.AddJsonBody(assetIds);
+			IHttpRestResponse<List<ConfigurationGroupDetail>> response = await ExecuteAsync<List<ConfigurationGroupDetail>>(request).ConfigureAwait(false);
+			return response.Data;
+		}
+
+		public List<ConfigurationGroupDetail> GetConnectedPeripheralsForAssets(long groupId, List<long> assetIds)
+		{
+			IHttpRestRequest request = GetRequest(APIControllerRoutes.DEVICECONFIGCONTROLLER.GETCONNECTEDPERIPHERALSFORASSETS, HttpMethod.Post);
+			request.AddJsonBody(assetIds);
+			IHttpRestResponse<List<ConfigurationGroupDetail>> response = Execute<List<ConfigurationGroupDetail>>(request);
+			return response.Data;
+		}
+
+	}
+}
