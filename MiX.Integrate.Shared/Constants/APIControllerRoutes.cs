e24e93aba6fda008f19fcdbbf8e4808a3a0c72c6
alainh <Alain.Hendrikse@mixtelematics.com>
2017-12-16 01:43:36 +0200

    MI-479: Locations changed since

--- MiX.Integrate.Shared/Constants/APIControllerRoutes.cs
+++ MiX.Integrate.Shared/Constants/APIControllerRoutes.cs
@@ -160,6 +160,7 @@ namespace MiX.Integrate.Shared.Constants
 			public const string DELETE = "api/locations/group/{groupId}/location/{locationId}";
 			public const string INRANGE = "api/locations/group/{groupId}/inrange/{meters}";
 			public const string NEAREST = "api/locations/group/{groupId}/nearest";
+			public const string CHANGEDSINCE = "api/locations/organisation/{organisationId}/changedsince/since/{since}";
 
 		}
 
