2fecf29b6459e334c422b7ef7feb605b8786c154
Johannes Engelbrecht <Johannes.Engelbrecht@mixtelematics.com>
2018-01-17 14:15:13 +0000

    Removed  commented code

--- MiX.Integrate.API.Client/Base/AccessTokenCache.cs
+++ MiX.Integrate.API.Client/Base/AccessTokenCache.cs
@@ -10,14 +10,7 @@ namespace MiX.Integrate.Api.Client.Base
 	{
 		private static Dictionary<int, TokenResponse> _idServerAccessTokens = new Dictionary<int, TokenResponse>();
 		private static object _lock = new object();
-
-		//Debug vars
-		private static string _classId = DateTime.UtcNow.ToString("HH:mm:ss.fff");
-		private static int _totalRequests = 0;
-		private static int _totalCleared = 0;
-		private static int _totalCacheResults = 0;
-
-
+ 
 		public static string GetIdServerAccessToken(IdServerResourceOwnerClientSettings settings)
 		{
 			lock (_lock)
@@ -31,17 +24,13 @@ namespace MiX.Integrate.Api.Client.Base
 
 				if (_idServerAccessTokens.ContainsKey(settingsHash) && _idServerAccessTokens[settingsHash] != null)
 				{
-					TokenResponse response = _idServerAccessTokens[settingsHash];
-					_totalCacheResults++;
-					//Console.WriteLine($"Return cached AccessToken #{_totalCacheResults} for {settingsHash} @ {_classId}");
+					TokenResponse response = _idServerAccessTokens[settingsHash]; 
 					//ToDo: possibly preemptively check if token is still valid 
 					return response.AccessToken;
 				}
 
 				try
-				{
-					_totalRequests++;
-					//Console.WriteLine($"GetIdServerAccessToken #{_totalRequests} for {settingsHash} @ {_classId}");
+				{ 
 
 					IdentityClient identityClient = new IdentityClient(settings.BaseAddress, settings.ClientId, settings.ClientSecret);
 					TokenResponse reponse = identityClient.RequestToken(settings.UserName, settings.Password, settings.Scopes);
@@ -53,8 +42,6 @@ namespace MiX.Integrate.Api.Client.Base
 
 					_idServerAccessTokens.Add(settingsHash, reponse);
 					 
-					//Console.WriteLine($"AccessToken #{_totalRequests} cached for {settingsHash} @ {_classId}");
-
 					return reponse.AccessToken;
 				}
 				catch (Exception exc)
@@ -74,10 +61,7 @@ namespace MiX.Integrate.Api.Client.Base
 				}
 
 				int settingsHash = $"{settings.BaseAddress}|{settings.ClientId}|{settings.ClientSecret}|{settings.Scopes}|{settings.UserName}|{settings.Password}".GetHashCode();
-
-				_totalCleared++;
-				//Console.WriteLine($"ClearIdServerAccessToken #{_totalCleared} for {settingsHash} @ {_classId}");
-
+ 
 				if (_idServerAccessTokens.ContainsKey(settingsHash))
 				{
 					_idServerAccessTokens.Remove(settingsHash);
