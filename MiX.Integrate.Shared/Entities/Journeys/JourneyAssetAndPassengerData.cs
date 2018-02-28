3800e4bb28babf500bd93dfa8bdadb4efde7ac53
Jonathan Healing <JonathanH@mixtelematics.com>
2018-02-28 10:44:10 +0200

    Add new url and clases

---
+++ MiX.Integrate.Shared/Entities/Journeys/JourneyAssetAndPassengerData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MiX.Integrate.Shared.Entities.Journeys
+{
+    public class JourneyAssetAndPassengerData
+    {
+		public List<JourneyAsset> JourneyAssets { get; set; }
+		public List<JourneyAssetPassenger> JourneyAssetPassengerList { get; set; }
+		public List<JourneyAssetExternalPassenger> JourneyAssetExternalPassengerList { get; set; }
+	}
+}
