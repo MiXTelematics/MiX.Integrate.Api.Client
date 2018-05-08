4c3d6ecd01454924efb3169fcee746bf0a16c20b
walter <walter.vanrooyen@mixtelematics.com>
2018-05-08 14:18:01 +0200

    JM-2177: created new journey route locations class

---
+++ MiX.Integrate.Shared/Entities/Journeys/JourneyRouteLocations.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MiX.Integrate.Shared.Entities.Journeys
+{
+	public class JourneyRouteLocations
+	{
+		public double TotalTimeInMinutes { get; set; }
+		public double TotalDistanceInMetres { get; set; }
+		public List<JourneyRouteLocation> RouteLocations { get; set; }
+	}
+}
