52f5f03c35d47c632ce4d5624de12d6e22694983
Jako de Wet <jakodewet@gmail.com>
2018-07-11 14:51:15 +0200

    Merge GHOS/Feature/TE-501 to Integration

---
+++ MiX.Integrate.Shared/Entities/TimeApprovers/DriverAllowedApprovers.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace MiX.Integrate.Shared.Entities.TimeApprovers
+{
+	public class DriverAllowedApprovers
+	{
+		public DriverAllowedApprovers()
+		{
+			ApproverIds = new List<string>();
+		}
+		public string HosDriverId { get; set; }
+		public List<string> ApproverIds { get; set; }
+	}
+}
