57ca2c158b1805203f2ded2d1f36fdbdedec2b44
Rudolph Gerber <Rudolph.Gerber@mixtelematics.com>
2018-09-14 15:06:30 +0200

    MI-658

--- MiX.Integrate.API.Client/MessagesClient.cs
+++ MiX.Integrate.API.Client/MessagesClient.cs
@@ -105,19 +105,21 @@ namespace MiX.Integrate.Api.Client
 			return response.Data;
 		}
 
-		public async Task<SendMessageResult> SendJobMessageAsync(long organisationId, SendJobMessageCarrier jobMessageCarrier)
+		public async Task<SendMessageResult> SendJobMessageAsync(long organisationId, SendJobMessageCarrier jobMessageCarrier, bool saveOnly)
 		{
 			IHttpRestRequest request = GetRequest(APIControllerRoutes.MESSAGESCONTROLLER.SENDJOBMESSAGE, HttpMethod.Post);
 			request.AddUrlSegment("organisationId", organisationId.ToString());
+			request.AddQueryParameter("saveOnly", saveOnly.ToString());
 			request.AddJsonBody(jobMessageCarrier);
 			IHttpRestResponse<SendMessageResult> response = await ExecuteAsync<SendMessageResult>(request).ConfigureAwait(false);
 			return response.Data;
 		}
 
-		public SendMessageResult SendJobMessage(long organisationId, SendJobMessageCarrier jobMessageCarrier)
+		public SendMessageResult SendJobMessage(long organisationId, SendJobMessageCarrier jobMessageCarrier, bool saveOnly)
 		{
 			IHttpRestRequest request = GetRequest(APIControllerRoutes.MESSAGESCONTROLLER.SENDJOBMESSAGE, HttpMethod.Post);
 			request.AddUrlSegment("organisationId", organisationId.ToString());
+			request.AddQueryParameter("saveOnly", saveOnly.ToString());
 			request.AddJsonBody(jobMessageCarrier);
 			IHttpRestResponse<SendMessageResult> response = Execute<SendMessageResult>(request);
 			return response.Data;
