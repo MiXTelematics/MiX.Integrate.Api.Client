575ef5597c741565c171f2a5edf307e1a8293227
Jako de Wet <jako.dewet@mixtelematics.com>
2018-10-02 13:31:45 +0200

    Push TE-565-1813 for Api.Client Build

---
+++ MiX.Integrate.API.Client/HosDataClient.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using MiX.Integrate.Api.Client;
+using MiX.Integrate.Api.Client.Base;
+using MiX.Integrate.Shared.Constants;
+using MiX.Integrate.Shared.Entities.HosData;
+
+namespace MiX.Integrate.API.Client
+{
+	public class HosDataClient : BaseClient, IHosDataClient
+	{
+		public HosDataClient(string url, bool setTestRequestHeader = false) : base(url, setTestRequestHeader) { }
+		public HosDataClient(string url, IdServerResourceOwnerClientSettings settings, bool setTestRequestHeader = false) : base(url, settings, setTestRequestHeader) { }
+
+		public List<HosViolation> GetHosViolations(long driverId, DateTime fromDateTime, DateTime toDateTime)
+		{
+			IHttpRestRequest request = GetRequest(APIControllerRoutes.HOSDATACONTROLLER.GETHOSVIOLATIONS, HttpMethod.Get);
+
+			request.AddUrlSegment("driverId", driverId.ToString());
+			request.AddUrlSegment("from", fromDateTime.ToString(DataFormats.DateTime_Format));
+			request.AddUrlSegment("from", fromDateTime.ToString(DataFormats.DateTime_Format));
+
+			IHttpRestResponse<List<HosViolation>> response = Execute<List<HosViolation>>(request);
+
+			return response.Data;
+		}
+
+		public async Task<List<HosViolation>> GetHosViolationsAsync(long driverId, DateTime fromDateTime, DateTime toDateTime)
+		{
+			IHttpRestRequest request = GetRequest(APIControllerRoutes.HOSDATACONTROLLER.GETHOSVIOLATIONS, HttpMethod.Get);
+
+			request.AddUrlSegment("driverId", driverId.ToString());
+			request.AddUrlSegment("from", fromDateTime.ToString(DataFormats.DateTime_Format));
+			request.AddUrlSegment("from", fromDateTime.ToString(DataFormats.DateTime_Format));
+
+			IHttpRestResponse<List<HosViolation>> response = await ExecuteAsync<List<HosViolation>>(request).ConfigureAwait(false);
+
+			return response.Data;
+		}
+	}
+}
---
+++ MiX.Integrate.API.Client/IHosDataClient.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using MiX.Integrate.Api.Client.Base;
+using MiX.Integrate.Shared.Entities.HosData;
+
+namespace MiX.Integrate.API.Client
+{
+	public interface IHosDataClient : IBaseClient
+	{
+		List<HosViolation> GetHosViolations(long driverId, DateTime fromDateTime, DateTime toDateTime);
+		Task<List<HosViolation>> GetHosViolationsAsync(long driverId, DateTime fromDateTime, DateTime toDateTime);
+	}
+}
