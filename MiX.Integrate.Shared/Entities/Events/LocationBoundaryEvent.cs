958d5b68a6d1e428a75f623dfe5f1fd794d8783a
Rudolph Gerber <Rudolph.Gerber@mixtelematics.com>
2018-10-24 10:18:21 +0200

    OL-144: Created carriers for request

---
+++ MiX.Integrate.Shared/Entities/Events/LocationBoundaryEvent.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MiX.Integrate.Shared.Entities.Events
+{
+	/// <summary>
+	/// Carrier for Location entered and Location left events
+	/// </summary>
+	[Serializable]
+	public class LocationBoundaryEvent : ServerSideEventBase
+	{
+		public List<long> Locations { get; set; }
+	}
+}
