466793a82a3fbba441df8fe16277d0ce5f1303a1
Jako de Wet <jako.dewet@mixtelematics.com>
2018-11-19 06:55:23 +0000

    Merged PR 21505: Merge Feature/TE-588 into Integration
    Merge Feature/TE-588 into Integration

---
+++ MiX.Integrate.Shared/Entities/HosData/HosEventStartDateTimeByHourChangedSince.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MiX.Integrate.Shared.Entities.HosData
+{
+	public class HosEventStartDateTimeByHourChangedSince
+	{
+		public HosEventStartDateTimeByHourChangedSince()
+		{
+			EventStartDateTimes = new List<DateTime>();
+		}
+		public long DriverId { get; set; }
+		public List<DateTime> EventStartDateTimes { get; set; }
+	}
+}
