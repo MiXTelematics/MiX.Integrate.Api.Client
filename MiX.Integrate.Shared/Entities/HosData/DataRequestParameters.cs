be56c113738064fd539e30ed13512b22bb3132ff
nicholas <nicholas.mei@mixtelematics.com>
2019-07-01 16:20:05 +0200

    changed request type to DataRequestParameters

---
+++ MiX.Integrate.Shared/Entities/HosData/DataRequestParameters.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace MiX.Integrate.Shared.Entities.HosData
+{
+	public class DataRequestParameters
+	{
+		public ParameterEntityType EntityTypeId { get; set; }
+		public List<long> EntityIds { get; set; }
+	}
+}
