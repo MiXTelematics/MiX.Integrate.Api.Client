f3ea5e2226635d2f01dca44bcd923d364f638897
Eugene Bauling <Eugene.Bauling@mixtelematics.com>
2019-11-05 14:31:38 +0200

    MI-763: Rename ClipID to MediaReference

--- MiX.Integrate.Shared/Entities/Events/EventClip.cs
+++ MiX.Integrate.Shared/Entities/Events/EventClip.cs
@@ -7,7 +7,7 @@ namespace MiX.Integrate.Shared.Entities.Events
 	public class EventClip
 	{
 		public long EventId { get; set; }
-		public string ClipId { get; set; }
+		public string MediaReference { get; set; }
 		public long AssetId { get; set; }
 	}
 }
