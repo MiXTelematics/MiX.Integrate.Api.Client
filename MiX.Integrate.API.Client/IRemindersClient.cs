35aa63b51cbd9272ceec61072449d8df464dc965
alainh <Alain.Hendrikse@mixtelematics.com>
2019-12-17 08:29:21 +0200

    MI-792: Get asset reminders by group

--- MiX.Integrate.API.Client/IRemindersClient.cs
+++ MiX.Integrate.API.Client/IRemindersClient.cs
@@ -1,4 +1,5 @@
-﻿using MiX.Integrate.API.Client.Base;
+﻿using System.Collections.Generic;
+using MiX.Integrate.API.Client.Base;
 using MiX.Integrate.Shared.Entities.Reminders;
 using System.Threading.Tasks;
 
@@ -10,10 +11,17 @@ namespace MiX.Integrate.API.Client
 		Task UpdateAssetLicenceReminderAsync(long organisationId, AssetLicenceReminder assetLicenceReminder);
 		Task UpdateAssetServiceReminderAsync(long organisationId, AssetServiceReminder assetServiceReminder);
 		Task UpdateAssetRoadworthyCertificateReminderAsync(long organisationId, AssetRoadworthyCertificateReminder assetRoadworthyCertificateReminder);
+		Task<List<AssetServiceReminder>> GetAssetServiceRemindersAsync(long groupId);
+		Task<List<AssetLicenceReminder>> GetAssetLicenceRemindersAsync(long groupId);
+		Task<List<AssetRoadworthyCertificateReminder>> GetAssetRoadworthyCertificateRemindersAsync(long groupId);
 
 		AssetReminders GetAssetReminders(long organisationId, long assetId);
 		void UpdateAssetLicenceReminder(long organisationId, AssetLicenceReminder assetLicenceReminder);
 		void UpdateAssetServiceReminder(long organisationId, AssetServiceReminder assetServiceReminder);
 		void UpdateAssetRoadworthyCertificateReminder(long organisationId, AssetRoadworthyCertificateReminder assetRoadworthyCertificateReminder);
+		List<AssetServiceReminder> GetAssetServiceReminders(long groupId);
+		List<AssetLicenceReminder> GetAssetLicenceReminders(long groupId);
+		List<AssetRoadworthyCertificateReminder> GetAssetRoadworthyCertificateReminders(long groupId);
+
 	}
 }
--- MiX.Integrate.API.Client/RemindersClient.cs
+++ MiX.Integrate.API.Client/RemindersClient.cs
@@ -1,4 +1,5 @@
-﻿using MiX.Integrate.API.Client.Base;
+﻿using System.Collections.Generic;
+using MiX.Integrate.API.Client.Base;
 using MiX.Integrate.Shared.Constants;
 using MiX.Integrate.Shared.Entities.Reminders;
 using System.Net.Http;
@@ -47,6 +48,39 @@ namespace MiX.Integrate.API.Client
 			await ExecuteAsync(request).ConfigureAwait(false);
 		}
 
+		/// <summary>Retrieves asset service reminders for assets in the specified group and its descendants.</summary>
+		/// <param name="groupId">Identifies the <see cref="MiX.Integrate.Shared.Entities.Groups.Group">Group</see> to search,
+		/// which must be an OrganisationGroup, OrganisationSubgroup, SubGroup, or DefaultSite.</param>
+		public async Task<List<AssetServiceReminder>> GetAssetServiceRemindersAsync(long groupId)
+		{
+			IHttpRestRequest request = GetRequest(APIControllerRoutes.RemindersController.GETASSETSERVICEREMINDERSFORGROUP, HttpMethod.Get);
+			request.AddUrlSegment("groupId", groupId.ToString());
+			IHttpRestResponse<List<AssetServiceReminder>> response = await ExecuteAsync<List<AssetServiceReminder>>(request).ConfigureAwait(false);
+			return response.Data;
+		}
+
+		/// <summary>Retrieves asset licence reminders for assets in the specified group and its descendants.</summary>
+		/// <param name="groupId">Identifies the <see cref="MiX.Integrate.Shared.Entities.Groups.Group">Group</see> to search,
+		/// which must be an OrganisationGroup, OrganisationSubgroup, SubGroup, or DefaultSite.</param>
+		public async Task<List<AssetLicenceReminder>> GetAssetLicenceRemindersAsync(long groupId)
+		{
+			IHttpRestRequest request = GetRequest(APIControllerRoutes.RemindersController.GETASSETLICENCEREMINDERSFORGROUP, HttpMethod.Get);
+			request.AddUrlSegment("groupId", groupId.ToString());
+			IHttpRestResponse<List<AssetLicenceReminder>> response = await ExecuteAsync<List<AssetLicenceReminder>>(request).ConfigureAwait(false);
+			return response.Data;
+		}
+
+		/// <summary>Retrieves asset roadworthy certificate reminders for assets in the specified group and its descendants.</summary>
+		/// <param name="groupId">Identifies the <see cref="MiX.Integrate.Shared.Entities.Groups.Group">Group</see> to search,
+		/// which must be an OrganisationGroup, OrganisationSubgroup, SubGroup, or DefaultSite.</param>
+		public async Task<List<AssetRoadworthyCertificateReminder>> GetAssetRoadworthyCertificateRemindersAsync(long groupId)
+		{
+			IHttpRestRequest request = GetRequest(APIControllerRoutes.RemindersController.GETASSETROADWORTHYREMINDERSFORGROUP, HttpMethod.Get);
+			request.AddUrlSegment("groupId", groupId.ToString());
+			IHttpRestResponse<List<AssetRoadworthyCertificateReminder>> response = await ExecuteAsync<List<AssetRoadworthyCertificateReminder>>(request).ConfigureAwait(false);
+			return response.Data;
+		}
+
 
 		public AssetReminders GetAssetReminders(long organisationId, long assetId)
     {
@@ -82,6 +116,39 @@ namespace MiX.Integrate.API.Client
 			request.AddUrlSegment("assetId", assetRoadworthyCertificateReminder.AssetId.ToString());
 			request.AddJsonBody(assetRoadworthyCertificateReminder);
 			Execute(request);
+		}
+
+		/// <summary>Retrieves asset service reminders for assets in the specified group and its descendants.</summary>
+		/// <param name="groupId">Identifies the <see cref="MiX.Integrate.Shared.Entities.Groups.Group">Group</see> to search,
+		/// which must be an OrganisationGroup, OrganisationSubgroup, SubGroup, or DefaultSite.</param>
+		public List<AssetServiceReminder> GetAssetServiceReminders(long groupId)
+		{
+			IHttpRestRequest request = GetRequest(APIControllerRoutes.RemindersController.GETASSETSERVICEREMINDERSFORGROUP, HttpMethod.Get);
+			request.AddUrlSegment("groupId", groupId.ToString());
+			IHttpRestResponse<List<AssetServiceReminder>> response =  Execute<List<AssetServiceReminder>>(request);
+			return response.Data;
+		}
+
+		/// <summary>Retrieves asset licence reminders for assets in the specified group and its descendants.</summary>
+		/// <param name="groupId">Identifies the <see cref="MiX.Integrate.Shared.Entities.Groups.Group">Group</see> to search,
+		/// which must be an OrganisationGroup, OrganisationSubgroup, SubGroup, or DefaultSite.</param>
+		public List<AssetLicenceReminder> GetAssetLicenceReminders(long groupId)
+		{
+			IHttpRestRequest request = GetRequest(APIControllerRoutes.RemindersController.GETASSETLICENCEREMINDERSFORGROUP, HttpMethod.Get);
+			request.AddUrlSegment("groupId", groupId.ToString());
+			IHttpRestResponse<List<AssetLicenceReminder>> response = Execute<List<AssetLicenceReminder>>(request);
+			return response.Data;
+		}
+
+		/// <summary>Retrieves asset roadworthy certificate reminders for assets in the specified group and its descendants.</summary>
+		/// <param name="groupId">Identifies the <see cref="MiX.Integrate.Shared.Entities.Groups.Group">Group</see> to search,
+		/// which must be an OrganisationGroup, OrganisationSubgroup, SubGroup, or DefaultSite.</param>
+		public List<AssetRoadworthyCertificateReminder> GetAssetRoadworthyCertificateReminders(long groupId)
+		{
+			IHttpRestRequest request = GetRequest(APIControllerRoutes.RemindersController.GETASSETROADWORTHYREMINDERSFORGROUP, HttpMethod.Get);
+			request.AddUrlSegment("groupId", groupId.ToString());
+			IHttpRestResponse<List<AssetRoadworthyCertificateReminder>> response = Execute<List<AssetRoadworthyCertificateReminder>>(request);
+			return response.Data;
 		}
 	}
 }
