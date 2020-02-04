a060ec2502e01b9e5650442195bb133b552a610f
Christo Boshoff <christo.boshoff@mixtelematics.com>
2020-02-04 16:27:40 +0200

    FE-412

--- MiX.Integrate.API.Client/ITripsClient.cs
+++ MiX.Integrate.API.Client/ITripsClient.cs
@@ -43,8 +43,8 @@ namespace MiX.Integrate.API.Client
 
 		CreatedSinceResult<Trip> GetCreatedSinceForOrganisation(long organisationId, string sinceToken, int quantity, bool includeSubTrips = false);
 		Task<CreatedSinceResult<Trip>> GetCreatedSinceForOrganisationAsync(long organisationId, string sinceToken, int quantity, bool includeSubTrips = false);
-		void UpdateTripClassification(long tripId, TripClassificationCategory classification);
-		Task UpdateTripClassificationAsync(long tripId, TripClassificationCategory classification);
+		void UpdateTripClassification(long tripId, TripClassification classification);
+		Task UpdateTripClassificationAsync(long tripId, TripClassification classification);
 		void UpdateTripClassificationComment(long tripId, string comment);
 		Task UpdateTripClassificationCommentAsync(long tripId, string comment);
 	}
