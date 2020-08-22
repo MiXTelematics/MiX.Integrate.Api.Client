15347730f8f80eff126f8680e7282ccf090cf348
Alain Hendrikse <alain.hendrikse@mixtelematics.com>
2020-08-22 06:24:15 +0200

    MI-806: Clean up Message properties

--- MiX.Integrate.Shared/Entities/Messages/Message.cs
+++ MiX.Integrate.Shared/Entities/Messages/Message.cs
@@ -10,19 +10,11 @@ namespace MiX.Integrate.Shared.Entities.Messages
 
 		public Message()
 		{
-			this.ID = Guid.NewGuid().ToString();
 			this.State = MessageStates.New;
 			this.Transport = CommsTransports.Default;
 			this.SetMinutesToLive(24 * 60);
-			this.Starts = new DateTime(1753, 01, 01); // SqlDateTime.MinValue.Value
 			this.Notes = string.Empty;
-			this.StateLastChanged = new DateTime(1753, 01, 01); // SqlDateTime.MinValue.Value
 			this.Parameters = string.Empty;
-			this.ActiveMessage = null;
-			this.Expires = new DateTime(1753, 01, 01); // SqlDateTime.MinValue.Value
-			this.DatePended = new DateTime(1753, 01, 01); // SqlDateTime.MinValue.Value
-			this.Processed = new DateTime(1753, 01, 01); // SqlDateTime.MinValue.Value
-			this.LastActionDate = new DateTime(1753, 01, 01); // SqlDateTime.MinValue.Value
 		}
 
 		#endregion Constructor
@@ -31,15 +23,8 @@ namespace MiX.Integrate.Shared.Entities.Messages
 
 		public int MessageId { get; set; }
 		public long AssetId { get; set; }
-		public string ID { get; set; }
 		public long OrgID { get; set; }
-		public int CompanyID { get; set; }
 		public short RecipientID { get; set; }
-		public string RecipientAddr { get; set; }
-		public string VehicleDescription { get; set; }
-		public string VehicleRegNo { get; set; }
-		public short DriverID { get; set; }
-		public string DriverName { get; set; }
 		public int ActiveMessageID { get; set; }
 		public MessageStates State { get; set; }
 		public MessageTypes MessageType { get; set; }
@@ -48,14 +33,9 @@ namespace MiX.Integrate.Shared.Entities.Messages
 		public MessageOrigins Origin { get; set; }
 		public DateTime? Expires { get; set; }
 		public DateTime? Starts { get; set; }
-		public DateTime DatePended { get; set; }
-		public DateTime LastActionDate { get; set; }
 		public string Notes { get; set; }
-		public DateTime StateLastChanged { get; set; }
-		public ActiveMessage ActiveMessage { get; set; }
-		public string VehicleDesc { get; set; }
 		public string Parameters { get; set; }
-		public DateTime Processed { get; set; }
+		public DateTime? Processed { get; set; }
 
 		#endregion Public Properties
 
