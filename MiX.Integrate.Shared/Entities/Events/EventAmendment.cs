2ee7a3325485d8f705e66f8b45dda8856d290145
Alain Hendrikse <alain.hendrikse@mixtelematics.com>
2020-09-28 12:11:27 +0200

    MI-825: Include updated driver ID in amendment data for reassigned trips and events

--- MiX.Integrate.Shared/Entities/Events/EventAmendment.cs
+++ MiX.Integrate.Shared/Entities/Events/EventAmendment.cs
@@ -5,14 +5,17 @@ namespace MiX.Integrate.Shared.Entities.Events
 	/// <summary>Details occurrences of amendments to events</summary>
 	public class EventAmendment
 	{
-		// Identifier of the amended event
+		/// <summary>Identifier of the amended event<summary></summary>
 		public long EventId { get; set; }
 
-		// The type of amendment made
+		/// <summary>The type of amendment made<summary></summary>
 		public AmendmentType AmendmentType { get; set; }
 
-		// Date and time of the amendment
+		/// <summary>Date and time of the amendment</summary>
 		public DateTime AmendmentDate { get; set; }
+
+		/// <summary>The identifier of the driver to which the event was assigned if <see cref="AmendmentType"/> is ReassignedEvent, otherwise null</summary>
+		public long? NewDriverId { get; set; } = null;
 	}
 }
 
--- MiX.Integrate.Shared/Entities/Trips/TripAmendment.cs
+++ MiX.Integrate.Shared/Entities/Trips/TripAmendment.cs
@@ -5,13 +5,16 @@ namespace MiX.Integrate.Shared.Entities.Trips
 	/// <summary>Details occurrences of amendments to trips</summary>
   public class TripAmendment
   {
-	  // Identifier of the amended trip
+	  /// <summary>Identifier of the amended trip</summary>
 	  public long TripId { get; set; }
 
-	  // The type of amendment made
+	  /// <summary>The type of amendment made</summary>
 	  public AmendmentType AmendmentType { get; set; }
 
-	  // Date and time of the amendment
+	  /// <summary>Date and time of the amendment</summary>
 	  public DateTime AmendmentDate { get; set; }
+
+	  /// <summary>The identifier of the driver to which the trip was assigned if <see cref="AmendmentType"/> is ReassignedTrip, otherwise null</summary>
+	  public long? NewDriverId { get; set; } = null;
   }
 }
