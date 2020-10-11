c95bdcfeff568bcc56e1ff2ead3b845e05310b38
Alain Hendrikse <alain.hendrikse@mixtelematics.com>
2020-10-11 20:38:11 +0200

    MI-824: Geospatial data file client WIP

---
+++ MiX.Integrate.Shared/Entities/GeoData/GeoDataFormat.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace MiX.Integrate.Shared.Entities.GeoData
+{
+	// Geospatial data output format
+	public enum GeoDataFormat : byte
+	{
+		GeoJson = 0,
+		Kml = 1,
+		Wkt =2
+	}
+}
