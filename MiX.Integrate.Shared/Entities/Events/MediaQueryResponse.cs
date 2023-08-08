fca5934a9e57331152edadfbefbdb6c0e943866f
Alain Hendrikse <alain.hendrikse@mixtelematics.com>
2023-08-08 10:32:32 +0200

    Add ThumbnailUrls to MediaQueryResponse

--- MiX.Integrate.Shared/Entities/Events/MediaQueryResponse.cs
+++ MiX.Integrate.Shared/Entities/Events/MediaQueryResponse.cs
@@ -5,5 +5,7 @@ namespace MiX.Integrate.Shared.Entities.Events
 	public class MediaQueryResponse : AssetMediaReference
 	{
 		public List<string> MediaUrls { get; set; }
+
+		public List<string> ThumbnailUrls { get; set; }
 	}
 }
