58e71fba8a5b24b318c00535ab3bb111284cb98e
Alain Hendrikse <alain.hendrikse@mixtelematics.com>
2023-11-28 12:19:27 +0200

    Set IsNullable attribute for XML serializer

--- MiX.Integrate.Shared/Entities/AEMP/EquipmentSnapshot.cs
+++ MiX.Integrate.Shared/Entities/AEMP/EquipmentSnapshot.cs
@@ -7,23 +7,23 @@ namespace MiX.Integrate.Shared.Entities.Aemp
 	public class EquipmentSnapshot
 	{
 		[XmlElement(Order = 10)] public EquipmentHeader EquipmentHeader { get; set; }
-		[XmlElement(Order = 20)] public Location Location { get; set; }
-		[XmlElement(Order = 30)] public CumulativeOperatingHours CumulativeOperatingHours { get; set; }
-		[XmlElement(Order = 40)] public FuelUsed FuelUsed { get; set; }
-		[XmlElement(Order = 50)] public FuelUsedLast24 FuelUsedLast24 { get; set; }
-		[XmlElement(Order = 60)] public Distance Distance { get; set; }
+		[XmlElement(Order = 20, IsNullable = false)] public Location Location { get; set; }
+		[XmlElement(Order = 30, IsNullable = false)] public CumulativeOperatingHours CumulativeOperatingHours { get; set; }
+		[XmlElement(Order = 40, IsNullable = false)] public FuelUsed FuelUsed { get; set; }
+		[XmlElement(Order = 50, IsNullable = false)] public FuelUsedLast24 FuelUsedLast24 { get; set; }
+		[XmlElement(Order = 60, IsNullable = false)] public Distance Distance { get; set; }
 
-		[XmlElement(Order = 110)] public CumulativeIdleHours CumulativeIdleHours { get; set; }
-		[XmlElement(Order = 120)] public FuelRemaining FuelRemaining { get; set; }
-		[XmlElement(Order = 130)] public DEFRemaining DEFRemaining { get; set; }
-		[XmlElement(Order = 140)] public EngineStatus EngineStatus { get; set; }
+		[XmlElement(Order = 110, IsNullable = false)] public CumulativeIdleHours CumulativeIdleHours { get; set; }
+		[XmlElement(Order = 120, IsNullable = false)] public FuelRemaining FuelRemaining { get; set; }
+		[XmlElement(Order = 130, IsNullable = false)] public DEFRemaining DEFRemaining { get; set; }
+		[XmlElement(Order = 140, IsNullable = false)] public EngineStatus EngineStatus { get; set; }
 
-		[XmlElement(Order = 210)] public CumulativePowerTakeOffHours CumulativePowerTakeOffHours { get; set; }
-		[XmlElement(Order = 220)] public AverageLoadFactorLast24 AverageLoadFactorLast24 { get; set; }
-		[XmlElement(Order = 230)] public CumulativeLoadCount CumulativeLoadCount { get; set; }
-		[XmlElement(Order = 240)] public CumulativePayloadTotals CumulativePayloadTotals { get; set; }
-		[XmlElement(Order = 250)] public CumulativeActiveRegenerationHours CumulativeActiveRegenerationHours { get; set; }
-		[XmlElement(Order = 260)] public CumulativeIdleNonOperatingHours CumulativeIdleNonOperatingHours { get; set; }
+		[XmlElement(Order = 210, IsNullable = false)] public CumulativePowerTakeOffHours CumulativePowerTakeOffHours { get; set; }
+		[XmlElement(Order = 220, IsNullable = false)] public AverageLoadFactorLast24 AverageLoadFactorLast24 { get; set; }
+		[XmlElement(Order = 230, IsNullable = false)] public CumulativeLoadCount CumulativeLoadCount { get; set; }
+		[XmlElement(Order = 240, IsNullable = false)] public CumulativePayloadTotals CumulativePayloadTotals { get; set; }
+		[XmlElement(Order = 250, IsNullable = false)] public CumulativeActiveRegenerationHours CumulativeActiveRegenerationHours { get; set; }
+		[XmlElement(Order = 260, IsNullable = false)] public CumulativeIdleNonOperatingHours CumulativeIdleNonOperatingHours { get; set; }
 
 	}
 }
