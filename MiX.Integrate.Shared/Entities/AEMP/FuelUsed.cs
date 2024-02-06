c5026df9ce71cec20ba0178dd9299fdb28b0a8aa
Alain Hendrikse <alain.hendrikse@mixtelematics.com>
2024-02-06 11:17:32 +0200

    AEMP: Add UOM fields for fuel, distance, payload, and tank metrics; apply XML tag name corrections

--- MiX.Integrate.Shared/Entities/AEMP/FuelUsed.cs
+++ MiX.Integrate.Shared/Entities/AEMP/FuelUsed.cs
@@ -9,6 +9,7 @@ namespace MiX.Integrate.Shared.Entities.Aemp
 		[XmlAttribute("dateTime")] public DateTime Datetime { get; set; }
 
 		/// <example>74</example>
-		[XmlElement] public uint FuelConsumedLitres { get; set; }
+		[XmlElement(ElementName="FuelConsumed")] public uint FuelConsumedLitres { get; set; }
+		[XmlElement] public string FuelUnits { get; set; } = "litres";
 	}
 }
