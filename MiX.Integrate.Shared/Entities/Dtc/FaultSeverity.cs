f6df3aa4677e7f1857b6b191af2f4608c71d4f86
Alain Hendrikse <alain.hendrikse@mixtelematics.com>
2024-02-22 23:29:07 +0200

    Add Dtc endpoints and additional asset identifier fields

---
+++ MiX.Integrate.Shared/Entities/Dtc/FaultSeverity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MiX.Integrate.Shared.Entities.Dtc
+{
+	public enum FaultSeverity : byte
+	{
+		Info = 0,
+		Low = 1,
+		Medium = 2,
+		High = 3,
+		Critical = 4
+	}
+}
---
+++ MiX.Integrate.Shared/Entities/Dtc/FaultStatus.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MiX.Integrate.Shared.Entities.Dtc
+{
+	/// <summary>Current status of a DTC fault for an asset</summary>
+	public class FaultStatus
+	{
+		/// <summary>Identifier of the asset reporting the fault</summary>
+		public long AssetId { get; set; }
+
+		/// <summary>Date and time the status was last updated</summary>
+		public DateTime Timestamp { get; set; }
+
+		/// <summary>DTC fault code</summary>
+		public string FaultCode { get; set; }
+
+		/// <summary>Fault category</summary>
+		public string Category { get; set; }
+
+		/// <summary>Fault description</summary>
+		public string Description { get; set; }
+
+		/// <summary>The severity of the fault</summary>
+		public FaultSeverity Severity { get; set; }
+
+		/// <summary><see langword="true" /> if the fault is open, <see langword="false"/> if the fault was resolved</summary>
+		public bool IsFaulted { get; set; }
+
+		/// <summary>Date and time the DTC was first observed</summary>
+		public DateTime? FirstSeen { get; set; }
+
+		/// <summary>Date and time the DTC was last observed</summary>
+		public DateTime? LastSeen { get; set; }
+
+		/// <summary>Number of times the fault was reported</summary>
+		public int Count { get; set; }
+
+		/// <summary>Fault symptom</summary>
+		public string Symptom { get; set; }
+
+		/// <summary>Action to be taken</summary>
+		public string Action { get; set; }
+
+		/// <summary>Date and time the fault was resolved</summary>
+		public DateTime? DateCleared { get; set; }
+	}
+}
